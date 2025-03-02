b5fb9f57b519caf1ae70739f51935442f9115825
cjwattttts <cameron.jwatts@outlook.com>
2025-03-02 17:19:04 -0500

    Debug

--- cc_12.js
+++ cc_12.js
@@ -58,11 +58,12 @@ document.addEventListener('DOMContentLoaded', () => {
             productItem.parentNode.removeChild(productItem); // removes clicked product item from list
         })
         
-        console.log('Product removed', itemToRemove.textContent);
+        console.log('Product removed', productItem.textContent);
 
         return productItem; // returns product item
+    }
 
-    console.log('Product added', newProductItem.textContent);
+    console.log('Product added', productItem.textContent);
 
     const inventoryList = document.getElementById("inventoryList"); // selects inv list container
 
