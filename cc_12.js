b767f50c2ebc3d0962f33a341027a31b8dde7cc4
cjwattttts <cameron.jwatts@outlook.com>
2025-03-02 16:10:43 -0500

    Debug

--- cc_12.js
+++ cc_12.js
@@ -1,24 +1,35 @@
 //TASK 1
 document.addEventListener('DOMContentLoaded', () => {
 
+function createCard(titleText, valueText) {
+    const card = document.createElement("div");
+    card.setAttribute("class", "metric-card"); // assigns class
+
+    const title = document.createElement('h3');
+    title.textContent = titleText; // sets title
+
+    const value = document.createElement('p');
+    value.textContent = valueText; // sets value
+
+    card.appendChild(title); // appends title to metric card
+    card.appendChild(value); // appends value to metric card
+
+    return card;
+}
+
 const dashboardContainer = document.getElementById("dashboard"); // selects dashboard container
 const altDashboardSelection = document.querySelector("#dashboard"); // alternative way
 
-const revenueCard = document.createElement("div"); 
-revenueCard.setAttribute("class", "metric-card"); // assigns class
-revenueCard.setAttribute("id", "revenueCard"); // assigns id
+const revenueCard = createCard("Revenue", "$0");
+dashboardContainer.appendChild(revenueCard);
 
-const title = document.createElement('h3');
-title.textContent = 'Revenue'; // sets the title
+const profitCard = createCard("Profit", "$0");
+dashboardContainer.appendChild(profitCard);
 
-const value = document.createElement('p');
-value.textContent = '$0'; // sets the placeholder value
+const expensesCard = createCard("Expenses", "$0")
+dashboardContainer.appendChild(expensesCard);
 
-revenueCard.appendChild(title); // appends title to metric
-revenueCard.appendChild(value); // appends value to metric
-
-dashboardContainer.appendChild(revenueCard); // appends metric to dashboard
-console.log('Added revenue card to dashboard');
+console.log('Added revenue, expenses, and profit card to dash');
 
 //TASK 2 
 
