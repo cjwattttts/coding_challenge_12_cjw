4fd7bab11119c1b4ef54d3ccbe609bd233f8b167
cjwattttts <cameron.jwatts@outlook.com>
2025-03-02 15:23:21 -0500

    Task 1 - Created Revenue Metric Card

---
+++ cc_12.js
@@ -0,0 +1,23 @@
+//TASK 1
+
+const dashboardContainer = document.getElementById("dashboard"); // selects dashboard container
+const altDashboardSelection = document.querySelector("#dashboard"); // alternative way
+
+const revenueCard = document.createElement("div"); 
+revenueCard.setAttribute("class", "metric-card"); // assigns class
+revenueCard.setAttribute("id", "revenueCard"); // assigns id
+
+const title = document.createElement("p");
+title.textConetnt = "Revenue"; // sets the title
+
+const value = document.createElement("p");
+title.textContent = "$0"; // sets the placeholder value
+
+revenueCard.appendChild(title); // appends title to metric
+revenueCard.appendChild(value); // appends value to metric
+
+dashboard.appendChild(revenueCard); // appends metric to dashboard
+
+
+
+
